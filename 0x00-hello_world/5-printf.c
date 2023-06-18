0cbefafea54af4e3cba1a2154977b1aeae736be7
Shawky <eng.shawky.ahmed@msn.com>
2023-06-18 13:53:00 -0700

    C0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: Write a C program that prints with printf
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
