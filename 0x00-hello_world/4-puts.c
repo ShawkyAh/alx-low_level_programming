22791feb1ae943ecf4d45e63baf6f72833b11a1c
Shawky <eng.shawky.ahmed@msn.com>
2023-06-18 13:42:38 -0700

    C0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+
+/**
+ * main- Entry point
+ *
+ * Description : C program that print function with puts
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
