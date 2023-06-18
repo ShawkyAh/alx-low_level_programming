cb5457a339149d9dbe5a5a8f0f634166783abe5d
Shawky <eng.shawky.ahmed@msn.com>
2023-06-18 15:35:03 -0700

    C1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *  Descreption: print alphabet lettersin lowercase reverse
+ *
+ *  Return: Alawys 0 (Success)
+ */
+
+int main(void)
+{
+char al = 'z';
+
+while (al >= 'a')
+{
+putchar(al);
+al++;
+}
+putchar('\n');
+return (0);
+}
