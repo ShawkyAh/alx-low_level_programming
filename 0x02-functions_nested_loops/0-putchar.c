641c3de1c2b00dadb4ec93cbd4e8c33586cce2ba
Shawky <eng.shawky.ahmed@msn.com>
2023-06-21 14:36:56 -0700

    C1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: prints _putcher prototype
- * 
+ *
  * Return: Always 0 (Success
 */
 
@@ -12,8 +12,9 @@ int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
+
 	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
-		_putchar('\n');
-	return (0);
-})	
+	_putchar('\n');
+return (0);
+}
