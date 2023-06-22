c540ba1ce9ab7d0c1e17a3efb232e94e823a082a
Shawky <eng.shawky.ahmed@msn.com>
2023-06-21 19:55:03 -0700

    C1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -35,7 +35,7 @@ return (l);
 int main(void)
 {
 	int count, ini;
-unsigned long f1 = 0, f2 = 1, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
 for (count = 1; count <= 98; count++)
 {
