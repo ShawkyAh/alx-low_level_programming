1cbae34a9bb59b2ceb560532f5d0041304097b49
Shawky <eng.shawky.ahmed@msn.com>
2023-06-21 18:33:46 -0700

    C1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,10 @@ int num, mult, prod;
 
 if (n <= 15 && n >= 0)
 {
-for (num = 0; num <= n; num++)
+for (num = 0; num <= n; ++num)
 {
 _putchar(48);
-for (mult = 1; mult <= n; mult++)
+for (mult = 1; mult <= n; ++mult)
 {
 _putchar(',');
 _putchar(' ');
@@ -28,7 +28,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
-_putchar((prod / 100) % 10 + 48);
+_putchar((prod / 10) % 10 + 48);
 }
 else if (prod <= 99 && prod >= 10)
 _putchar((prod / 10) + 48);
