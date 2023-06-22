4b8a6b959948c8d462f86162b1460b422b1a3b80
Shawky <eng.shawky.ahmed@msn.com>
2023-06-21 17:13:03 -0700

    C1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,11 +3,11 @@
 
 /**
  * _putchar - write the char c to stdout
- * @c: the char to  print
+ * @c: the character to  print
  *
  * Return: on success 1.
  * on error -1 is returned, and error is set to appear
- */
+*/
 
 int _putchar(char c)
 {
