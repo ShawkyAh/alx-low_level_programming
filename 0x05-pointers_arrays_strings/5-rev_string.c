a9d6db424156d12f4ee868ee6d8f4bfac4e44390
Shawky <eng.shawky.ahmed@msn.com>
2023-06-27 19:29:52 -0700

    C5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,16 +11,16 @@
 
 void rev_string(char *s)
 {
-int l, a;
-char t;
+int l, i;
+char temp;
 
-for (l = 0; s[1] != '\0'; ++l)
+for (l = 0; s[l] != '\0'; ++l)
 ;
 
-for (a = 0; a < l / 2; a++)
+for (i = 0; i < l / 2; i++)
 {
-t = s[a];
-s[a] = s[l - 1 - a];
-s[l - 1 - a] = t;
+temp = s[i];
+s[i] = s[l - 1 - i];
+s[l - 1 - i] = temp;
 }
 }
