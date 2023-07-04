914c0bfe456e3b4280d37fce09c32e0059a3d7f0
Shawky <eng.shawky.ahmed@msn.com>
2023-07-04 07:32:19 -0700

    C7

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 {
-unsigned int i;
+unsigned int s;
 
-for (i = 0; i < n; i++)
+for (s = 0; s < n; s++)
 {
-dest[i] = src[i];
+dest[s] = src[s];
 }
 return (dest);
 }
