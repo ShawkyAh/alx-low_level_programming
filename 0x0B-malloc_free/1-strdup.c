174afdf9bce683631325ee6d79e0fef0045b2d73
Shawky <eng.shawky.ahmed@msn.com>
2023-07-11 10:01:04 -0700

    CB

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,14 +11,15 @@
 char *_strdup(char *str)
 {
 int i = 0, size = 0;
-char *m
+char *m;
 
 if (str == NULL)
 return (NULL);
+
 for (; str[size] != '\0'; size++)
 ;
 
-m = malloc(size * sizeof(*str) + 1);
+m = malloc(size * sizeof((*str) + 1));
 
 if (m == 0)
 {
@@ -29,6 +30,5 @@ else
 for (; i < size; i++)
 m[i] = str[i];
 }
-
 return (m);
 }
