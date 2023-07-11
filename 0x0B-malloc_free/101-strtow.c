d29c423038b210cf960039c86720a9ce951ea345
Shawky <eng.shawky.ahmed@msn.com>
2023-07-11 10:35:24 -0700

    CB

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -38,21 +38,20 @@ int i, j, k, l, n = 0, wc = 0;
 char **w;
 
 if (str == NULL || *str == '\0')
-{
 return (NULL);
-}
+
 n = wrdcnt(str);
+
 if (n == 1)
-{
 return (NULL);
-}
-w = (char **)malloc(n * sizeof(char *));
+
+w = (char **)malloc(n *sizeof(char *));
+
 if (w == NULL)
-{
 return (NULL);
-}
 w[n - 1] = NULL;
 i = 0;
+
 while (str[i])
 {
 if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
@@ -60,15 +59,15 @@ if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
-w[wc] = (char *)malloc(j * sizeof(char));
+w[wc] = (char *)malloc(j *sizeof(char));
 j--;
 if (w[wc] == NULL)
 {
 for (k = 0; k < wc; k++)
 {
 free(w[k]);
-}
-free(w[n - 1);
+
+free(w[n - 1]);
 free(w);
 return (NULL);
 }
