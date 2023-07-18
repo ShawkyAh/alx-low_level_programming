bb35b3e9150a299da6bc7a15a6f1546d3bb98814
Shawky <eng.shawky.ahmed@msn.com>
2023-07-17 18:49:42 -0700

    CE

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-printf("Name: %s\n", d->name ? d->name : "(nil)";
-printf("Age: %s\n", d->age;
-printf("Owner: %s\n", d->Owner ? d->Owner : "(nil)";
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
