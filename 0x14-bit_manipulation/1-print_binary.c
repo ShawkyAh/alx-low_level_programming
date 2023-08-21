fc0a268a8c7a61db3ae536967502dd07eb4109f5
Shawky <eng.shawky.ahmed@msn.com>
2023-08-21 16:48:32 -0700

    C14

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_binary - converts int to binary
+*@n: the int number to ptint
+*Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+int x = sizeof(n) * 8, printed = 0;
+
+while (x)
+{
+if (n & 1L << --bit)
+{
+_putchar('1');
+printed++;
+}
+else if (printed)
+_putchar('0');
+}
+if (!printed)
+_putchar('0');
+}
