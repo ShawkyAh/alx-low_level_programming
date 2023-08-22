88ef07297fb3bed56b97fe1b4225357bbd818e3a
Shawky <eng.shawky.ahmed@msn.com>
2023-08-22 06:00:12 -0700

    C15

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,7 +14,7 @@ int i;
 
 printf("  Magic:   ");
 for (i = 0; i < EI_NIDENT; i++)
-printf("%2.2x%s", h.e_ident[i], i == EI_NIDENT - 1 ? "\n" : "");
+printf("%2.2x%s", h.e_ident[i], i == EI_NIDENT - 1 ? "\n" : " ");
 }
 
 /**
