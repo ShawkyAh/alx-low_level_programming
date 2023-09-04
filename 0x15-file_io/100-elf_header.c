21ca8b654db9226ef62cc764c6bb3d3ad9ab0973
Shawky <eng.shawky.ahmed@msn.com>
2023-09-04 05:33:37 -0700

    New

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -255,11 +255,11 @@ if (ac != 2)
 dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
 fd = open(av[1], O_RDONLY);
 if (fd == -1)
-dprintf(STDERR_FILENO, "Can`t open file: %s\n", av[1]), exit(98);
+dprintf(STDERR_FILENO, "Can't open file: %s\n", av[1]), exit(98);
 b = read(fd, &h, sizeof(h));
 if (b < 1 || b != sizeof(h))
-dprintf(STDERR_FILENO, "Can`t read from file: %s\n", av[1]), exit(98);
-if (h.e_ident[0] == 0x7f && h.e_ident[1] == 'E' && h.e_ident[2] == 'L' &&
+dprintf(STDERR_FILENO, "Can't read from file: %s\n", av[1]), exit(98);
+	if (h.e_ident[0] == 0x7f && h.e_ident[1] == 'E' && h.e_ident[2] == 'L' &&
 h.e_ident[3] == 'F')
 {
 printf("ELF Header:\n");
